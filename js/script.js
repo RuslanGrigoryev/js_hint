496c3e3d8ba008e9f9919838d6fb164621d8422b
RuslanGrigoryev <rus.grigoryev@yandex.ua>
2014-07-23 11:08:33 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function argsTest (_a, _b, _c) {
+	console.log(arguments.length);//длина массива передаваемых аргументов
+	console.log(arguments[1]);// второй элемент этого массива
+} 
+argsTest(12, 33, 52);
