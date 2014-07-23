705422481396f42d8eb3237d9e18fad535290955
RuslanGrigoryev <rus.grigoryev@yandex.ua>
2014-07-23 11:16:40 +0300

    проверка на правильное количество аргументов

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 function argsTest (_a, _b, _c) {
-	console.log(arguments.length);//длина массива передаваемых аргументов
+	var l = arguments.length;
+	if ( l != 3 ) {// проверка на правильное количество аргументов
+		throw new Error("function called with" + l + " args, but you need 3");
+	}
+	console.log(l);//длина массива передаваемых аргументов
 	console.log(arguments[1]);// второй элемент этого массива
 } 
 argsTest(12, 33, 52);
