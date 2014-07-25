29692588028e228c8b902b04010262fdf00d17f5
RuslanGrigoryev <rus.grigoryev@yandex.ua>
2014-07-25 13:44:47 +0300

    delay before loading

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function argsTest (_a, _b, _c) {
+/*function argsTest (_a, _b, _c) {
 	var l = arguments.length;
 	if ( l != 3 ) {// проверка на правильное количество аргументов
 		throw new Error("function called with" + l + " args, but you need 3");
@@ -18,3 +18,34 @@ function max () {
 	return m;
 }
 console.log( max(23, 51, 26, 345) );
+
+function fact(x) {
+	if (x <= 1) return 1;
+	return x*arguments.callee(x-1);// свойство, ссылающееся на исполняемую на данный момент - функцию
+}
+
+console.log(fact(8));
+
+(function () {
+	var selectedText = '';
+	    document.onmouseup = function() {
+	        setTimeout(function() {
+	            if (window.getSelection) {
+	                var selection = window.getSelection();
+	                selectedText = selection.toString();
+	    			getLastSelection();
+
+	            } else if (document.selection) {
+	                var range = document.selection.createRange();
+	                selectedText = range.htmlText;
+	   				getLastSelection();
+
+	            }
+	        }, 10);
+	    }
+	  
+	    window.getLastSelection = function() {
+	        console.log(selectedText);
+	    }
+
+})();*/
